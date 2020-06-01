278013a1758b298c402922bcf1fa9aef6588d7e5
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-01 21:15:13 +0200

    added tests

---
+++ Assets/Tests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void BasicTest()
+        {
+
+            bool isActive = false;
+
+            Assert.AreEqual(false, isActive);
+
+        }
+
+        [Test]
+        public void CatchingErrors()
+        {
+
+            GameObject gameObject = new GameObject("test");
+
+            Assert.Throws<MissingComponentException>(
+                () => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one
+            );
+
+        }
+    }
+}
